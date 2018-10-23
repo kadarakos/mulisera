ca3f2017623ad4dd7be8c99f835e7c62f48e526e
Akos Kadar <u1257964@green.uvt.nl>
2018-10-23 14:17:24 +0200

    full pipeline from raw coco

--- coco_process.py
+++ coco_process.py
@@ -2,7 +2,8 @@ import json
 import sys
 import os
 from collections import defaultdict
-
+import argparse
+from img_feat_extract import extract_img_feats
 
 def extract(root, output_path, split="val"):
     '''
@@ -34,18 +35,23 @@ def extract(root, output_path, split="val"):
         ids2files[i['id']] = i['file_name']
     
     sortedids = sorted(ids2files.keys())
-    
+    imgs_path = os.path.join(coco_root, '{}{}'.format(split, 2014))
+
     '''
     Write the names of the files onto disk (filenames.txt),
     write the mapping of imageID:filename to disk (ids2file.txt)
     '''
     handle0 = open(os.path.join(output_path, '{}_filenames.txt'.format(split)), 'w')
     handle2 = open(os.path.join(output_path, "{}_ids2files.txt".format(split)), "w")
+    index =   open(os.path.join(imgs_path, 'index.txt'), 'w')
+ 
     for i in sortedids:
         handle0.write("{}\n".format(ids2files[i]))
+        index.write("{}\n".format(ids2files[i]))
         handle2.write('{}:{}\n'.format(i, ids2files[i]))
     handle0.close()
     handle2.close()
+    index.close()
     
     '''
     Collect the captions for each image. These are stored in a 
@@ -76,10 +82,24 @@ def extract(root, output_path, split="val"):
     print("Ignored {} captions because there were more than 5 captions for a given image".format(ignored))
 
 if __name__ == "__main__":
-    coco_root = sys.argv[1]
-    output_path = sys.argv[2]
+    parser = argparse.ArgumentParser(prog='extract-cnn-features')
+    parser.add_argument('-r', '--coco_root', type=str, required=True,
+                        help='Folder to image files i.e. /images/train')
+    parser.add_argument('-b', '--batch-size', type=int, default=32,
+                        help='Batch size for forward pass.')
+    parser.add_argument('-o', '--output_path', type=str, default='resnet50',
+                        help='Path to output directory.')
+
+    # Parse arguments
+    args = parser.parse_args()
+    coco_root = args.coco_root
+    output_path = args.output_path
     if not os.path.exists(output_path):
         os.mkdir(output_path)
-
+    # Create text files to handle caption-image correspondances
     extract(coco_root, output_path, "val")
     extract(coco_root, output_path, "train")
+    # Extract image features
+    extract_img_feats(coco_root, "val", args.batch_size, output_path)
+    extract_img_feats(coco_root, "train", args.batch_size, output_path)
+
